d69da01f9ba617213c6e34ca77612fefdbe6017c
Andrea Wu <1359259+andreaowu@users.noreply.github.com>
2022-11-10 10:42:40 -0500

    add mvp finished code

--- mvp/firebase/firebase.js
+++ mvp/firebase/firebase.js
@@ -22,13 +22,7 @@ import { getStorage } from 'firebase/storage';
 
 // Configure Firebase.
 const firebaseConfig = {
-  apiKey: "AIzaSyB8xMPCyes3GGvxz0-ig2jbwqdpBDKB1Rk",
-  authDomain: "expensetracker-5501a.firebaseapp.com",
-  projectId: "expensetracker-5501a",
-  storageBucket: "expensetracker-5501a.appspot.com",
-  messagingSenderId: "989666135103",
-  appId: "1:989666135103:web:a69eef7ce191eb52dffc8a",
-  measurementId: "G-YVP679EBHY"
+  // Insert your Firebase project's configuration here
 };
 
 export const app = initializeApp(firebaseConfig);
