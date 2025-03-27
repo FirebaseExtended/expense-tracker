63ffad41c442249e851fad8e420303dabf10ff70
Quynh Nguyen <quynhnh@gmail.com>
2025-03-27 22:57:33 +0000

    Add Firebase package & config

--- mvp-starter/firebase/firebase.js
+++ mvp-starter/firebase/firebase.js
@@ -14,3 +14,24 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDhuK7uQ0o4kQoPxzRlJxzlh5lo-kk341w",
+  authDomain: "expense-tracker-5b8ce.firebaseapp.com",
+  projectId: "expense-tracker-5b8ce",
+  storageBucket: "expense-tracker-5b8ce.firebasestorage.app",
+  messagingSenderId: "996920118886",
+  appId: "1:996920118886:web:66a94e9a6511ba6bd0a7a7",
+  measurementId: "G-569SGXPD9W"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
